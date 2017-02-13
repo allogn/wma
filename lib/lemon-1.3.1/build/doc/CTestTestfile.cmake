b34242e2cbb97d4658988338e1332fc8e14b797f
Alvis Logins <alvis.logins@gmail.com>
2017-02-13 11:10:16 +0100

    removed build lemon

--- lib/lemon-1.3.1/build/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alvis/PhD/fcla/lib/lemon-1.3.1/doc
-# Build directory: /Users/alvis/PhD/fcla/lib/lemon-1.3.1/build/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
