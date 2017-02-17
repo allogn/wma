7000980defad0b6a611a37f37eeffacfb5b732d5
Alvis Logins <alvis.logins@gmail.com>
2017-02-17 16:26:54 +0100

    script for geometric graph generation

---
+++ scripts/generate_geometric.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for SIZE in {10..200..20};
+do
+    for SOURCE_NUM in {1..10..1};
+    do
+        echo ../bin/generator -n ${SIZE} -o ../data/geometric_${SIZE}_${SOURCE_NUM}.gr -g 0 -s ${SOURCE_NUM} --density 0.1
+        ../bin/generator -n ${SIZE} -o ../data/geometric_${SIZE}_${SOURCE_NUM}.gr -g 0 -s ${SOURCE_NUM} --density 0.1
+    done
+done
+echo "Generation done"
