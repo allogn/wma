c7bee83542c5cf96254a151abc0a489e1dc86cc8
Alvis Logins <alvis@cs.aau.dk>
2017-02-22 14:43:06 +0100

    ready to run first experiments

--- scripts/generate_geometric.sh
+++ scripts/generate_geometric.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
-for SIZE in {10..200..20};
+DATA_DIR=/q/storage/alogins/graphs/random_geometric
+for SIZE in {10..400..50};
 do
-    for SOURCE_NUM in {1..10..1};
+    for ((SOURCE_NUM = 1; SOURCE_NUM <= $SIZE; SOURCE_NUM+=10));
     do
-        echo ../bin/generator -n ${SIZE} -o ../data/geometric_${SIZE}_${SOURCE_NUM}.gr -g 0 -s ${SOURCE_NUM} --density 0.4
-        ../bin/generator -n ${SIZE} -o ../data/geometric_${SIZE}_${SOURCE_NUM}.gr -g 0 -s ${SOURCE_NUM} --density 0.4
+        ../bin/generator -n ${SIZE} -o ${DATA_DIR}/geometric_${SIZE}_${SOURCE_NUM}.gr -g 0 -s ${SOURCE_NUM} --density $(($SIZE/100))
     done
 done
 echo "Generation done"
