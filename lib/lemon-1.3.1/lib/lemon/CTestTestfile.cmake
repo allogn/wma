fd3400726eaeeba5a669c2c884ead0223166e018
Alvis Logins <alvis@cs.aau.dk>
2017-03-13 16:00:36 +0100

    exclude lemon from commit

--- lib/lemon-1.3.1/lib/lemon/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alogins/fcla/lib/lemon-1.3.1/lemon
-# Build directory: /home/alogins/fcla/lib/lemon-1.3.1/lib/lemon
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
